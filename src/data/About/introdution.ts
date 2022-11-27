7cc17eb049fa70ada038d736a5fef43aed6ef375
rikmyk <rikomaykel01@gmail.com>
2022-11-27 15:34:33 +0700

    update informations

--- src/data/About/introdution.ts
+++ src/data/About/introdution.ts
@@ -1,6 +1,6 @@
 export const introduction: string[] = [
   "Hi everyone, my name is Rico Maykel Erawanto. I'm a Junior Full Stack Developer, Information Systems Student, Gamers, and Google-fu.",
-  "I live in Medan, Indonesia. I was born in Bekasi, 15 May 2003. I'm 19 years old. I'm a Information Systems Student from Universitas Prima Indonesia.",
-  'Coding has been my passion and hobby since I was 16 years old, and I love computer since I was a kid. Besides coding, I love listening to music. My favorite genres are Hip-hop , and jazz. I like playing games too. My favorite genre is First Person Shooter, and Role Playing Game',
+  "I live in Medan, Indonesia. I was born in Bekasi, 15 May 2003. I'm 19 years old. I'm a Information Systems Student from Prima Indonesia University.",
+  'Coding has been my passion and hobby since I was 16 years old, and I love programming I was a kid. Besides coding, I love listening to music. My favorite genres are Hip-hop , and jazz. I like playing games too. My favorite genre is First Person Shooter, and Role Playing Game',
   'Sometimes I watch anime, but I prefer watching western movies and series, especially MCU (Marvel Cinematic Universe). I spend a lot of time on Facebook sharing memes and shitposts.',
 ];
--- src/data/About/workExperience.ts
+++ src/data/About/workExperience.ts
@@ -3,10 +3,11 @@ import { IWorkExperience } from './workExperience.d';
 export const workExperience: IWorkExperience[] = [
   {
     name: '-',
-    type: '-',
+    type: 'Freelance',
     tasks: [
-      '-',
+      'Deployed websites that have been finished by the team to web hosting service.',
+      'Published articles for client’s',
     ],
-    timePeriod: '-',
+    timePeriod: '2020 - 2022',
   },
 ];
