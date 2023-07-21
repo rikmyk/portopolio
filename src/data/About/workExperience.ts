399041a319ce3930e168feb7747d9748050ec6ad
rikmyk <rikomaykel01@gmail.com>
2023-07-21 19:32:22 +0700

    update resume

--- src/data/About/workExperience.ts
+++ src/data/About/workExperience.ts
@@ -1,13 +1,5 @@
 import { IWorkExperience } from './workExperience.d';
 
 export const workExperience: IWorkExperience[] = [
-  {
-    name: '-',
-    type: 'Freelance',
-    tasks: [
-      'Deployed websites that have been finished by the team to web hosting service.',
-      'Published articles for client’s',
-    ],
-    timePeriod: '2020 - 2022',
-  },
+  
 ];
