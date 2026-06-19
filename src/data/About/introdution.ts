aeebe002e7aa9106790a5041ef8d428a05fd5b27
rikmyk <rikomaykel01@gmail.com>
2026-06-19 13:57:19 +0700

    chore(data): update About Me introduction text content

--- src/data/About/introdution.ts
+++ src/data/About/introdution.ts
@@ -1,13 +1,13 @@
 export const introduction = {
   paragraphs: [
-    "Hi everyone, my name is Rico Maykel Erawanto. I'm a Junior Full Stack Developer, Information Systems Student, Gamers, and Google-fu.",
+    "Hi everyone! My name is Rico Maykel Erawanto. I'm a Junior Full Stack Developer, Information Systems Student, gamer, and Google-fu search enthusiast. Currently, I'm working as a Front End / Fullstack Developer Intern at PT Nose Herbal Indo, having previously built HRIS systems and APIs as a Full Stack Intern at Mahakarya Maritim Indonesia.",
     {
-      beforeAge: "I live in Medan, Indonesia. I was born in Bekasi, 15 May 2003. I'm ",
-      afterAge: " years old. I'm a Information Systems Student from Prima Indonesia University.",
+      beforeAge: "I live in Medan, Indonesia. I was born in Bekasi, 15 May 2003, which makes me ",
+      afterAge: " years old. I am currently pursuing my degree in Information Systems at Universitas Prima Indonesia.",
     },
-    'Coding has been my passion and hobby since I was 16 years old, and I love programming I was a kid. Besides coding, I love listening to music. My favorite genres are Hip-hop , and jazz. I like playing games too. My favorite genre is First Person Shooter, and Role Playing Game',
-    'Sometimes I watch anime, but I prefer watching western movies and series, especially MCU (Marvel Cinematic Universe). I spend a lot of time on Facebook sharing memes and shitposts.',
+    "Coding has been my passion and hobby since I was 16 years old. I enjoy building various backend solutions such as Discord automation bots, custom Roblox API community checkers, private digital transaction sites like BECdex, and news/weather applications with Flutter. I love designing relational databases, integrating payment gateways, and implementing clean code.",
+    "Besides coding, I love listening to music, especially Hip-hop and Jazz. I also enjoy playing games, with First-Person Shooters (FPS) and Role-Playing Games (RPG) being my favorite genres. In my free time, I watch anime, follow western series (especially the Marvel Cinematic Universe), and spend time sharing memes and tech discussions on Facebook.",
   ],
-  birthDate: { year: 2003, month: 5, day: 15 }, // May 15, 2006
+  birthDate: { year: 2003, month: 5, day: 15 }, // May 15, 2003
 };
 
